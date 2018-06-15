49604172ed072f4b8cf82e4b56648dc251322bd8
Savelii Ugrinhcuk <dudle1999@mail.ru>
2018-06-15 20:07:47 +0500

    Объявил основные переменные

--- Practic №2/Program.cs
+++ Practic №2/Program.cs
@@ -12,36 +12,6 @@ namespace Practic__2
     //Если 4 угла в одной плоскости есть пересечение
     //Если в разных - есть пересечение
 
-    class Line
-    {
-        public Point x;
-        public Point y;
-
-        public Line(Point x, Point y)
-        {
-            this.x = x;
-            this.y = y;
-        }
-
-    }
-
-    class Point
-    {
-        public double x;
-        public double y;
-
-        public Point()
-        {
-            x = 0;
-            y = 0;
-        }
-
-        public Point(int x, int y)
-        {
-            this.x = x;
-            this.y = y;
-        }
-    }
 
     class Program
     {
@@ -52,24 +22,7 @@ namespace Practic__2
             W = Convert.ToInt16(Console.ReadLine());
             E = Convert.ToInt16(Console.ReadLine());
             
-            Point FirstPoint = new Point(0,W);
-            Point SecondPoint = new Point(100*N,E);
-            Line line = new Line(FirstPoint,SecondPoint);
-
-            for (int i = 1; i <= N; i++)
-            {
-                for (int j = 1; j <= N; j++)
-                {
-                    Point p1 = new Point(100*i,100*j);
-                    Point p2 = new Point(100 * i, 100 * j - 100);
-                    Point p3 = new Point(100 * i - 100, 100 * j);
-                    Point p4 = new Point(100*i -100, 100*j -100);
-
-                    
-
-
-                }
-            }
+            
         }
     }
 }
