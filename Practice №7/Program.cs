4c65364a6407cf722346aa7a91ac5f0f91dd42e0
Savelii Ugrinhcuk <dudle1999@mail.ru>
2018-06-16 22:45:08 +0500

    Добавил счетчик кол-ва букв в слове

--- Practice №7/Program.cs
+++ Practice №7/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,40 @@ using System.Threading.Tasks;
 
 namespace Practice__7
 {
+     
+
     class Program
     {
+        /// <summary>
+        /// Получение кол-ва частоты букв
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        static Dictionary<char, int> FrequencyCounter(string str)
+        {
+            Dictionary<char, int> dictionary = new Dictionary<char, int>();
+
+            foreach (var symbol in str)                 
+            {
+                if (dictionary.ContainsKey(symbol))         //если в словаре уже есть такой символ
+                    dictionary[symbol]++;
+                else                                    //если символ найден впервые
+                    dictionary[symbol] = 1;
+            }
+
+            return dictionary;
+        }
+
         static void Main(string[] args)
         {
+            string str = "abfdcdscassaacfefwesd";
+
+            var frequencies = FrequencyCounter(str);
+
+            foreach (var item in frequencies)
+                Console.WriteLine(item.Key + " - " + item.Value);
+
+            Console.ReadLine();
         }
     }
 }
