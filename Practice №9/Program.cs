2d4c44fa39b833a9802933140a8eb243ee051116
Savelii Ugrinhcuk <dudle1999@mail.ru>
2018-06-17 19:48:54 +0500

    Решение задания 9

--- Practice №9/Program.cs
+++ Practice №9/Program.cs
@@ -7,12 +7,19 @@ using System.Threading.Tasks;
 namespace Practice__9
 {
 
+    /// <summary>
+    /// Линейный список
+    /// </summary>
     public class MyList
     {       
-        int data;
-        public MyList nextValue;
-        public int count = 0;
+        public int data;                //данные
+        public MyList nextValue;        //следующий элемент
+        static public int count = 0;    //счетчик
 
+        /// <summary>
+        /// Конструктор с первым (головным) элементом
+        /// </summary>
+        /// <param name="data"></param>
         public MyList(int data)
         {
             this.data = data;
@@ -20,46 +27,28 @@ namespace Practice__9
             count++;
         }
 
-        public int this[int index]
-        {
-            get
-            {
-                MyList current = this;
-
-                for (int i = 0; i <= index && current != null; i++)
-                    if (i == index)
-                        return current.data;
-                    else
-                        current = current.nextValue;
-
-                return -1;
-            }
-        }
-
+        /// <summary>
+        /// Создание списка с определенным количеством элементов
+        /// </summary>
+        /// <param name="N"></param>
+        /// <returns></returns>
         public static MyList AddElements(int N)
         {
-            MyList FirstElement = new MyList(1);
-            MyList current = FirstElement;
+            MyList FirstElement = new MyList(1);        //номер 1 становится первым(головным) элементом спискка
+            MyList Element = FirstElement;              //текущий элемент
 
-            for (int i = 2; i <= N; i++)
+            for (int i = 2; i <= N; i++)                
             {
-                current.nextValue =  new MyList(i);
-                current = current.nextValue;
+                Element.nextValue =  new MyList(i);
+                Element = Element.nextValue;            
             }
-
-            return FirstElement;
+            return FirstElement;                        //возвращаем список
         }
 
-        //public static MyList Remove(int index)
-        //{
-        //    Console.WriteLine("Введите номер удаляемого элемента:");
-        //    while (!int.TryParse(Console.ReadLine(), out index) || index < 0)
-        //        Console.WriteLine("Ошибка ввода, введите кол-во больше 0");
-
-        //    for(int i)
-
-        //}
-
+        /// <summary>
+        /// Перегрузка метода ToString для вывода элементов списка
+        /// </summary>
+        /// <returns></returns>
         public override string ToString()
         {
             return String.Format("Номер: " + data);
@@ -68,27 +57,154 @@ namespace Practice__9
 
     class Program
     {
-        static void Main(string[] args)
+
+        /// <summary>
+        /// Вывод списка
+        /// </summary>
+        /// <param name="list"></param>
+        static public void Show(MyList list)
         {
-
-            Console.WriteLine("Введите N (кол-во номеров в списке):");
-            int N;
-            while(!int.TryParse(Console.ReadLine(), out N) || N<1)
-                Console.WriteLine("Ошибка ввода, введите кол-во больше 0");
-            MyList list = MyList.AddElements(N);
-
-            for (int i = 0; i < N; i++)
+            Console.Write("Линейный список: ");
+            if (list == null)                               // если список пуст
+                Console.WriteLine("Список пуст");
+            else
             {
-                if (list != null)
+                MyList Element = list; // создание нового элемента и запись в него первого
+                while (Element != null) // цикл до конца списка
                 {
-                    Console.WriteLine(list.ToString());
-                    list = list.nextValue;
+                    Console.Write(Element.data + "  "); // вывод на экран текущего значения
+                    Element = Element.nextValue; // переход к след. элементу
                 }
+                Console.WriteLine("");
             }
+        }
 
-            Console.WriteLine(list[4]);
+        /// <summary>
+        /// Удаление элемента с определенным номером
+        /// </summary>
+        /// <param name="list"></param>
+        static public void Remove(ref MyList list) //удаление элемента 
+        {
+            if (list != null)
+            {
+                MyList Element = list;
+                MyList prev = list;
+                int i = 1;
 
-            Console.ReadLine();
+                Console.WriteLine("Введите номер элемента: ");
+                int pos;
+                while (!int.TryParse(Console.ReadLine(), out pos) || pos < 1)
+                    Console.WriteLine("Ошибка ввода, введите номер больше 0");
+                try
+                {
+                    while (Element.data != pos)
+                    {
+                        if (Element != null)
+                        {
+                            prev = Element;
+                            Element = Element.nextValue;
+                            i++;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+                catch (NullReferenceException)
+                {
+                   
+                }
+                if (list == Element)
+                    list = list.nextValue;
+                else if (Element != null)
+                {
+                    prev.nextValue = Element.nextValue;
+                }
+                else
+                {
+                    Console.WriteLine("Такого номера в списке нет");
+                }
+                MyList.count--;
+            }
+            else
+                Console.WriteLine("Список пуст");
+        }
+
+        /// <summary>
+        /// Поиск по значению
+        /// </summary>
+        /// <param name="list"></param>
+        static public void FindElement(MyList list)
+        {
+            bool ok = true;
+            Console.WriteLine("Введите номер который надо найти: ");
+            int number;
+            while (!int.TryParse(Console.ReadLine(), out number) || number < 1)
+                Console.WriteLine("Ошибка ввода, введите номер больше 0");
+
+            Console.WriteLine("Результаты поиска: ");
+            while (list != null)                                                //пока не дошли до конца списка
+            {
+                if (list.data == number)                                        //если элемент найден
+                {
+                    Console.Write(list.data);                                   //вывести найденный элемент
+                    ok = false;
+                }                    
+                list = list.nextValue;                                          //переход к след. элементу
+            }
+            if(ok)
+                Console.WriteLine("Номер не найден");
+        }
+
+        /// <summary>
+        /// Главное меню
+        /// </summary>
+        static public void Menu()
+        {
+            MyList list = null;
+            bool ok = true;
+            do
+            {
+                Console.WriteLine("\nМеню для работы с линейным списком:"
+                                  + "\n1.Создание списка"
+                                  + "\n2.Удаление выбранного элемента"
+                                  + "\n3.Поиск элемента"
+                                  + "\n4.Вывод списка"
+                                  + "\n5.Выход");
+                int choice;
+                while (!int.TryParse(Console.ReadLine(), out choice) || choice < 1 || choice > 5)
+                    Console.WriteLine("Ошибка ввода, выберите пункт (1-5)");
+
+                switch (choice)
+                {
+                    case 1:
+                        int N;
+                        Console.WriteLine("Введите N (кол-во номеров в списке):");
+                        while (!int.TryParse(Console.ReadLine(), out N) || N < 1)
+                            Console.WriteLine("Ошибка ввода, введите кол-во больше 0");
+                        list = MyList.AddElements(N);
+                        break;
+                    case 2:
+                        Remove(ref list);
+                        break;
+                    case 3:
+                        FindElement(list);
+                        break;
+                    case 4:
+                        Show(list);
+                        break;
+                    case 5:
+                        ok = false;
+                        break;
+                }
+            } while (ok == true);
+
+        }
+
+        static void Main(string[] args)
+        {                     
+            Menu();
         }
     }
 }
