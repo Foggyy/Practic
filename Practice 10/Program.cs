4575a2eb9400995e6d861064b6c3f26699c4cdd6
Savelii Ugrinhcuk <dudle1999@mail.ru>
2018-06-18 23:31:22 +0500

    fix

---
+++ Practice 10/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Zadacha
+{
+    class TwoWayList // двунаправленный список
+    {
+        public int dataTwo; //информационное поле
+
+        public TwoWayList nextTwo, //адрес следующего элемента
+            predTwo; //адрес предыдущего элемента
+
+        public TwoWayList()
+        {
+            dataTwo = 0;
+            nextTwo = null;
+            predTwo = null;
+        }
+
+        public TwoWayList(int d)
+        {
+            dataTwo = d;
+            nextTwo = null;
+            predTwo = null;
+        }
+
+        public void AddToEnd(int data) //добавление элементов списка (в конец)
+        {
+            if (nextTwo == null)
+            {
+                nextTwo = new TwoWayList(data);
+            }
+            else
+            {
+                nextTwo.AddToEnd(data);
+            }
+        }
+
+        public override string ToString()
+        {
+            return dataTwo + " ";
+        }
+
+
+        class Program
+        {
+            static TwoWayList VvodElemTwoList() //двунаправленный список, ввод элементов списка пользователем
+            {
+                int N;
+                int dataTwo;
+                Console.WriteLine("Введите сколько элементов вы хотите добавить в список");
+                while (!int.TryParse(Console.ReadLine(), out N) || N < 2)
+                    Console.WriteLine("Ошибка ввода. Введите целое число больше 1");
+
+                Console.WriteLine("Введите элементы списка");
+                while (!int.TryParse(Console.ReadLine(), out dataTwo))
+                    Console.WriteLine("Ошибка ввода. Введите целое число.");
+
+                TwoWayList myLList = new TwoWayList(dataTwo); //ввод первого элемента списка
+
+                for (int i = 0; i < N - 1; i++)
+                {
+                    while (!int.TryParse(Console.ReadLine(), out dataTwo))
+                        Console.WriteLine("Ошибка ввода. Введите целое число.");
+                    myLList.AddToEnd(dataTwo); //ввод остальных элементов списка
+                }
+                return myLList;
+            }
+
+            static void ShowList2(TwoWayList MyList)        //вывод двунаправленного списка
+            {
+
+                if (MyList == null)                         //проверка на пустой список
+                {
+                    Console.WriteLine("Список пуст.");
+                    return;
+                }
+                TwoWayList p = MyList;
+                while (p != null)
+                {
+                    Console.Write(p);
+                    p = p.nextTwo;                          //переход к следующему элементу
+                }
+                Console.WriteLine();
+            }
+
+            static public int Recurse(TwoWayList CurrentList , TwoWayList NextList)      //вычислить можно где-то здесь
+            {
+                int result = 0;                
+                while (CurrentList.nextTwo != null)
+                {
+                    NextList = CurrentList.nextTwo;
+                    result = CurrentList.dataTwo - CurrentList.nextTwo.dataTwo;
+
+                    result = 0;
+                }
+                
+                return result;
+            }
+
+            static void Main(string[] args)
+            {
+
+                TwoWayList List = new TwoWayList();
+                List = VvodElemTwoList();
+                ShowList2(List);
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Practice №11/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice__11
+{
+    class Program
+    {
+        /// <summary>
+        /// Шифрование последовательности
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="N"></param>
+        /// <returns></returns>
+        static public int[] Encrypt(int [] a, int N)
+        {
+            int[] b = new int[N];                                       //последовательность для хранения шифровки
+            Console.WriteLine("\nЗашифрованная последовательность:");
+            for (int i = 0; i < N; i++)
+            {
+                if (i == 0)
+                {
+                    b[i] = a[i];
+                }
+                else
+                if (a[i - 1] == a[i])
+                {
+                    b[i] = 1;
+                }
+                else
+                {
+                    b[i] = 0;
+                }
+
+                Console.Write(b[i] + " ");
+            }
+            return b;
+        }
+
+        /// <summary>
+        /// Возвращаем расшифрованный массив (в параметры передаем массив, содержащий значения элементов последовательности до шифрования)
+        /// </summary>
+        /// <param name="a"></param>
+        /// <returns></returns>
+        static public int[] Decrypt(int[] a, int N)
+        {
+            int[] b = new int[N];
+            Console.WriteLine("\nРасшифрованная последовательность:");
+            for (int i = 0; i < N; i++)
+            {
+                if (i == 0)
+                {
+                    b[i] = a[i];
+                }
+                if (i != N-1 && a[i] == a[i+1])
+                {
+                    b[i] = a[i];
+                }
+                else
+                {
+                    b[i] = a[i];
+                }
+                
+                Console.Write(b[i] + " ");
+            }
+            return b;
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину последовательности:");
+            int N;  
+            while(!int.TryParse(Console.ReadLine(), out N) || N<1)              
+                Console.WriteLine("Ошибка ввода, введите число не меньше 2");
+            int [] a = new int[N];
+                   
+            Console.WriteLine("Введите элементы последовательности:");
+            for (int i = 0; i < N; i++)
+            {
+                while (!int.TryParse(Console.ReadLine(), out a[i]))              
+                    Console.WriteLine("Ошибка ввода, введите целое число");
+            }
+            int[] b = Encrypt(a, N);                                            //шифрование
+            b = Decrypt(a, N);                                                  //расшифровка
+
+            Console.ReadLine();
+        }
+    }
+}
